af35ffa25181f1286377c5931db74216e513b59e
Vladimir Petrigo <vladimir.petrigo@gmail.com>
2026-06-22 12:36:45 +0200

    tui: Add session pagination support

--- packages/sdk/js/src/v2/gen/types.gen.ts
+++ packages/sdk/js/src/v2/gen/types.gen.ts
@@ -9449,6 +9449,7 @@ export type SessionListData = {
     path?: string
     roots?: boolean | "true" | "false"
     start?: number
+    cursor?: string
     search?: string
     limit?: number
   }
