b50fe2c27bd23b852df0f56f2ece2fd3ab3a7eea
Vladimir Petrigo <vladimir.petrigo@gmail.com>
2026-07-02 12:23:27 +0200

    tui: Remove duplication of locale specific functions

--- packages/tui/src/util/locale.ts
+++ packages/tui/src/util/locale.ts
@@ -16,10 +16,7 @@ export function datetime(input: number): string {
 
 export function datetimeFull(input: number): string {
   const date = new Date(input)
-  return date.toLocaleString(undefined, {
-    dateStyle: "medium",
-    timeStyle: "medium",
-  })
+  return `${String(date.getDate()).padStart(2, "0")}/${String(date.getMonth() + 1).padStart(2, "0")}/${date.getFullYear()} ${String(date.getHours()).padStart(2, "0")}:${String(date.getMinutes()).padStart(2, "0")}`
 }
 
 export function todayTimeOrDateTime(input: number): string {
